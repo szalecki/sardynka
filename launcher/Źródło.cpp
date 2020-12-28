1847bf621fe4bebc399aeeafdd9deb628c2820f7
szalecki <76705150+szalecki@users.noreply.github.com>
2020-12-28 20:43:17 +0100

    Add files via upload

---
+++ launcher/Źródło.cpp
@@ -0,0 +1,12 @@
+﻿#include <windows.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    HINSTANCE result;
+    result = ShellExecute(NULL, L"runas", L"jan.bat", NULL, NULL, SW_SHOWDEFAULT);//zamiast jan.bat będzie exe z wlasciwym programem(szyperbat)
+    if ((int)result <= 32)
+        std::cout << "Error!\nReturn value: " << (int)result << "\n";
+    return 0;
+}
